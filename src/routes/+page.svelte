52c6adcb573ee235629a821780a19550e311369c
PDominikHU <patkodominik0@gmail.com>
2026-02-13 12:03:00 +0100

    Still testing...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Kertfodrasz</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="http://kertfodrasz.hu">kertfodrasz.hu</a></p>
