b8f02838b77a7d7ab64e0d32013815093474fbe3
PDominikHU <patkodominik0@gmail.com>
2026-02-13 13:17:34 +0100

    Testing githooks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex min-h-screen flex-col items-center justify-center">
-	<h1>Kertfodrász</h1>
-	<h1 class="text-9xl ">WORK IN PROGRESS</h1>
+	<h1>Kertfodrász Kft.</h1>
+	<h1 class="text-9xl">WORK IN PROGRESS</h1>
 </div>
